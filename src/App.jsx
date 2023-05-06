3d21cd317f8f9095e3d6b224fdb47aa9d8b2f33c
natalievmedrano <nataliemed0403@gmail.com>
2023-05-06 10:48:10 -0500

    added mainloader + instantiated navbar/ images

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Dashboard, { dashboardLoader } from "./pages/Dashboard";
 import Error from "./pages/Error";
+import Main, { mainLoader } from "./layouts/Main";
+import { Children } from "react";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Main/>,
+    loader: mainLoader,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
     element: <Dashboard/>,
     loader: dashboardLoader,
     errorElement: <Error/>
+      }
+    ]
   },
   {
     path: "*",
