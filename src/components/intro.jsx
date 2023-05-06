c2d1aa80c2a44ac51e2ab99bb43cf4d2779a78ef
natalievmedrano <nataliemed0403@gmail.com>
2023-05-06 11:50:54 -0500

    working on login page

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,5 +1,30 @@
+import { Form } from "react-router-dom";
+import illustration from "../assets/illustration.jpg"
+
 const Intro = () => {
-    return ( <div>intro</div> );
-}
- 
+  return (
+    <div className="intro">
+      <div>
+        <h1>
+          Keep Track of <span className="accent"> Your Bills</span>
+        </h1>
+        <p>Budgeting and Tracking your bills is the key to financial freedom. Get started Today.</p>
+        <Form method="post">
+          <input
+            type="text"
+            name="userName"
+            required
+            placeholder="Enter your name"
+            aria-label="Your Name"
+          />
+          <button type="submit" className="btn btn--dark">
+            <span>create account</span>
+          </button>
+        </Form>
+      </div>
+      <img src={illustration} alt="" />
+    </div>
+  );
+};
+
 export default Intro;
